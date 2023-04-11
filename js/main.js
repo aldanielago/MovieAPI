5c672965563082a1e276335511711d7a2aff7834
Laura Aponte <aldanielago@gmail.com>
2023-04-11 06:37:07 -0500

    [Functionality] Receiving list of categories

--- js/main.js
+++ js/main.js
@@ -1,4 +1,6 @@
-URL_API = 'http://api.themoviedb.org/3/trending/movie/day'
+const trending_section = document.querySelector('#trending_preview .trending_preview-movieList');
+const categories_section = document.querySelector('#categories_preview .categories_preview-list');
+URL_API = 'http://api.themoviedb.org/3/';
 
 async function getTrendingMoviesPreview(){
     const res = await fetch('https://api.themoviedb.org/3/trending/movie/day?api_key=' + API_KEY);
@@ -6,8 +8,6 @@ async function getTrendingMoviesPreview(){
 
     const movies = data.results;
     movies.forEach(movie => {
-        const trending_section = document.querySelector('#trending_preview .trending_preview-movieList');
-
         const movie_container = document.createElement('div');
         movie_container.classList.add('movie-container');
 
@@ -20,4 +20,26 @@ async function getTrendingMoviesPreview(){
     });
 }
 
-getTrendingMoviesPreview()
+async function getCategoryPreview(){
+    const res = await fetch('https://api.themoviedb.org/3/genre/movie/list?api_key=' + API_KEY);
+    const data = await res.json();
+
+    const categories = data.genres;
+    categories.forEach(category => {
+        const category_container = document.createElement('div');
+        category_container.classList.add('category-container');
+
+        const category_title = document.createElement('h3');
+        category_title.classList.add('category-title');
+        category_title.setAttribute('id', 'id' + category.id);
+
+        const category_title_text = document.createTextNode(category.name);
+
+        category_title.appendChild(category_title_text);
+        category_container.appendChild(category_title);
+        categories_section.appendChild(category_container);
+    });
+}
+
+getTrendingMoviesPreview();
+getCategoryPreview();
